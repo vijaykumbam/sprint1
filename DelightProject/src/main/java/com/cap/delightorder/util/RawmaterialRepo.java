2a888a5b4c0c51eb2cd67ea3f939faa63d589282
vijaykumbam <37972916+vijaykumbam@users.noreply.github.com>
2020-02-15 11:24:01 +0530

    added

--- DelightProject/src/main/java/com/cap/delightorder/util/RawmaterialRepo.java
+++ DelightProject/src/main/java/com/cap/delightorder/util/RawmaterialRepo.java
@@ -14,11 +14,18 @@ public class RawmaterialRepo
 	public void setLlref(LinkedList<RawMaterialBean> llref) {
 		RawmaterialRepo.llref = llref;
 	}
+	
+	public void listmethod(LinkedList<RawMaterialBean> llref2) {
+		for(int i=0;i<2;i++)
+		{
+			System.out.println(llref2);
+		}
+	}
 
 	public boolean addRawMaterial(RawMaterialBean bean)
 	{
 		llref.add(bean);
-		System.out.println(llref);
+		listmethod(llref);
 		return true;
 	}
 }
